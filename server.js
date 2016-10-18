014c625f6c97419b946151651be69683d288e365
adventurebear <suzanneatkinson@gmail.com>
2016-10-17 23:38:40 -0400

    added moment.js

--- server.js
+++ server.js
@@ -1,43 +1,24 @@
 var express = require('express');
 var app = express();
 
+var moment = require('moment');
+moment().format();
+
+app.get('/:input', function( req, res){
+  var input = req.params;
+  var m = moment("input");
+  //console.log(input);
+  console.log("Valid Date?", m.isValid());
+  res.send(m);
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.put('/:time', function( req, res){
-  var time = req.params.time;
-  res.send(time);
-
-});
-
-
-
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Timeserver microapp listening on port 8080!');
 });
 
-
-/**
- * Created by suzanne on 10/16/16.
-
-
-var express = require('express');
-var app = express();
-
-var port = process.argv[2];
-
-app.put('/message/:id', function(req, res){
-  var id = req.params.id;
-  var hash = require('crypto')
-    .createHash('sha1')
-    .update(new Date().toDateString() + id)
-    .digest('hex');
-
-  res.send(hash);
-
-});
-
- */
 
 
