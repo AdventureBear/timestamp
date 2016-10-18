2ca0635a5acd5d657cd41336fa8d1720e1e2f38a
Justin Clagg <justinclagg@gmail.com>
2016-10-18 00:36:11 -0400

    added nodemon, fixed input variable

--- server.js
+++ server.js
@@ -4,21 +4,25 @@ var app = express();
 var moment = require('moment');
 moment().format();
 
-app.get('/:input', function( req, res){
-  var input = req.params;
-  var m = moment("input");
-  //console.log(input);
-  console.log("Valid Date?", m.isValid());
-  res.send(m);
-});
-
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/:input', function(req, res){
+  var input = req.params.input;
+  var date = moment(input);
+  var validDate = date.isValid();
+
+  console.log("Valid Date?", validDate);
+
+  if (validDate) {
+    res.send(date)
+  } else {
+    res.send("Invalid date");
+  }
+
 });
 
 app.listen(8080, function () {
   console.log('Timeserver microapp listening on port 8080!');
 });
-
-
-
